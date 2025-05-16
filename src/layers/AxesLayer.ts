646c75f50b35b8e7af1a40fe10f53ab50faada9d
Mantra <mantrakusumgar@outlook.com>
2025-05-16 01:15:27 +0100

    async zoom

--- src/layers/AxesLayer.ts
+++ src/layers/AxesLayer.ts
@@ -34,7 +34,7 @@ export class AxesLayer extends OptionalLayer {
       .attr("vector-effect", "non-scaling-stroke")
       .call(axisY);
 
-    this.zoom = () => {
+    this.zoom = async () => {
       const { animationDuration } = layerArgs.globals;
 
       axisLayerX.transition()
