dc838aae6268b05a6033954135400d550f85412a
Mantra <mantrakusumgar@outlook.com>
2025-06-02 15:54:11 +0100

    revert app demo change

--- src/demo/App.vue
+++ src/demo/App.vue
@@ -1,39 +1,10 @@
 <template>
-  <h1>Basic traces (spark lines)</h1>
-  <div class="chart" ref="chartSparkLines"></div>
-
-  <h1>Chart with axes</h1>
-  <div class="chart" ref="chartOnlyAxes"></div>
-
-  <h1>Axes and gridlines</h1>
-  <div class="chart" ref="chartAxesAndGrid"></div>
-
-  <h1>Axes, labels and gridlines</h1>
-  <div class="chart" ref="chartAxesLabelsAndGrid"></div>
-
-  <h1>Traces, gridlines, axes, labels and zoom</h1>
-  <div class="chart" ref="chartAxesLabelGridAndZoom"></div>
-
-  <h1>Chart with tooltips</h1>
-  <div class="chart" ref="chartTooltips"></div>
-
-  <h1>Responsive chart</h1>
-  <div class="chart-responsive" ref="chartResponsive"></div>
-
-  <h1>Stress test: 1000 traces</h1>
-  <button @click="drawStressChart">Draw</button>
-  <div class="chart" ref="chartStress"></div>
+  <div style="height: 55vh; width: 75vw;" ref="chart" class="bordered"></div>
 </template>
 
-<style>
-.chart {
-  width: 1000px;
-  height: 500px;
-}
-
-.chart-responsive {
-  width: 60vw;
-  height: 55vh;
+<style lang="css" scoped>
+.bordered {
+  border: solid black 1px;
 }
 </style>
 
@@ -41,59 +12,40 @@
 import { Chart, Lines, Scales } from "../skadi-chart";
 import { onMounted, ref } from "vue";
 
-const chartSparkLines = ref<HTMLDivElement | null>(null);
-const chartOnlyAxes = ref<HTMLDivElement | null>(null);
-const chartAxesAndGrid = ref<HTMLDivElement | null>(null);
-const chartAxesLabelsAndGrid = ref<HTMLDivElement | null>(null);
-const chartAxesLabelGridAndZoom = ref<HTMLDivElement | null>(null);
-const chartTooltips = ref<HTMLDivElement | null>(null);
-const chartResponsive = ref<HTMLDivElement | null>(null);
-const chartStress = ref<HTMLDivElement | null>(null);
+const chart = ref<HTMLDivElement | null>(null);
 
-const propsBasic = {
-  nX: 1000,
-  nL: 10,
-  ampScaling: 3,
-  freqRange: 0.1,
-  freqOffset: 0.95,
-  phaseRange: 0.5,
-  phaseOffset: 0.25,
-  opacityRange: 0.2,
-  opacityOffset: 0.8,
-  strokeWidthRange: 0.1,
-  strokeWidthOffset: 0.9,
-}
+// can safely ignore all generating code, it just allows me to easily make
+// pretty pictures
+// 
+// only important ones are nX and nL, number of points per line and number of
+// lines
+const nX = 1000;
+const nL = 1000;
 
-const propsStress = {
-  nX: 1000,
-  nL: 1000,
-  ampScaling: 3,
-  freqRange: 0.1,
-  freqOffset: 0.95,
-  phaseRange: 0.5,
-  phaseOffset: 0.25,
-  opacityRange: 0.15,
-  opacityOffset: 0.01,
-  strokeWidthRange: 0.4,
-  strokeWidthOffset: 0,
-}
+const ampScaling = 3;
+const freqRange = 0.1;
+const freqOffset = 0.95;
+const phaseRange = 0.5;
+const phaseOffset = 0.25;
+const opacityRange = 0.5;
+const opacityOffset = 0.2;
 
-const makeRandomCurves = (props: typeof propsBasic) => {
-  const xPoints = Array.from({length: props.nX + 1}, (_, i) => i / props.nX);
+const makeRandomCurves = () => {
+  const xPoints = Array.from({length: nX + 1}, (_, i) => i / nX);
   const lines: Lines = [];
   const makeYFunc = () => {
     const amp1 = Math.random();
     const amp2 = Math.random();
     const amp3 = Math.random();
     const amp4 = Math.random();
-    const freq1 = Math.random() * props.freqRange + props.freqOffset;
-    const freq2 = Math.random() * props.freqRange + props.freqOffset;
-    const freq3 = Math.random() * props.freqRange + props.freqOffset;
-    const freq4 = Math.random() * props.freqRange + props.freqOffset;
-    const phase1 = Math.random() * props.phaseRange + props.phaseOffset;
-    const phase2 = Math.random() * props.phaseRange + props.phaseOffset;
-    const phase3 = Math.random() * props.phaseRange + props.phaseOffset;
-    const phase4 = Math.random() * props.phaseRange + props.phaseOffset;
+    const freq1 = Math.random() * freqRange + freqOffset;
+    const freq2 = Math.random() * freqRange + freqOffset;
+    const freq3 = Math.random() * freqRange + freqOffset;
+    const freq4 = Math.random() * freqRange + freqOffset;
+    const phase1 = Math.random() * phaseRange + phaseOffset;
+    const phase2 = Math.random() * phaseRange + phaseOffset;
+    const phase3 = Math.random() * phaseRange + phaseOffset;
+    const phase4 = Math.random() * phaseRange + phaseOffset;
     return (x: number) => {
       return amp1 * Math.sin(freq1 * 53 * x + phase1)
         + amp2 * Math.cos(freq2 * 31 * x + phase2)
@@ -103,24 +55,32 @@ const makeRandomCurves = (props: typeof propsBasic) => {
   };
 
   // rainbow
-  const colors = [ "#e81416", "#ffa500", "#faeb36", "#79c314", "#487de7", "#4b369d", "#70369d" ];
+  const colors = [
+    "#e81416",
+    "#ffa500",
+    "#faeb36",
+    "#79c314",
+    "#487de7",
+    "#4b369d",
+    "#70369d"
+  ];
 
   const randomIndex = (length: number) => {
     return Math.floor(Math.random() * length);
   };
 
-  for (let l = 0; l < props.nL; l++) {
+  for (let l = 0; l < nL; l++) {
     const line: Lines[number] = {
       points: [],
       style: {
-        opacity: Math.random() * props.opacityRange + props.opacityOffset,
+        opacity: Math.random() * opacityRange + opacityOffset,
         color: colors[randomIndex(colors.length)],
-        strokeWidth: Math.random() * 1
+        strokeWidth: Math.random() * 0.5
       }
     };
     const yfunc = makeYFunc();
-    for (let i = 0; i < props.nX + 1; i++) {
-      line.points.push({ x: xPoints[i], y: yfunc(xPoints[i]) * props.ampScaling });
+    for (let i = 0; i < nX + 1; i++) {
+      line.points.push({ x: xPoints[i], y: yfunc(xPoints[i]) * ampScaling });
     }
     lines.push(line);
   }
@@ -128,68 +88,17 @@ const makeRandomCurves = (props: typeof propsBasic) => {
 };
 
 const tooltipHtmlCallback = (point: {x: number, y: number}) => {
-  return `X: ${point.x.toFixed(3)}, Y: ${point.y.toFixed(3)}`;
-};
-
-const curvesSparkLines = makeRandomCurves(propsBasic);
-const curvesOnlyAxes = makeRandomCurves(propsBasic);
-const curvesAxesAndGrid = makeRandomCurves(propsBasic);
-const curvesAxesLabelsAndGrid = makeRandomCurves(propsBasic);
-const curvesAxesLabelGridAndZoom = makeRandomCurves(propsBasic);
-const curvesTooltips = makeRandomCurves(propsBasic);
-const curvesResponsive = makeRandomCurves(propsBasic);
-const curvesStress = makeRandomCurves(propsStress);
-
-const scales: Scales = { x: {start: 0, end: 1}, y: {start: -10, end: 10} };
-
-const drawStressChart = () => {
-  new Chart(scales)
-    .addZoom()
-    .addTraces(curvesStress)
-    .addAxes()
-    .addTooltips(tooltipHtmlCallback)
-    .appendTo(chartStress.value!);
+  return `X: ${point.x}, Y: ${point.y}`;
 };
 
 onMounted(async () => {
+  const scales: Scales = { x: {start: 0, end: 1}, y: {start: -10, end: 10} };
   new Chart(scales)
-    .addTraces(curvesSparkLines)
-    .appendTo(chartSparkLines.value!);
-
-  new Chart(scales)
-    .addTraces(curvesOnlyAxes)
-    .addAxes()
-    .appendTo(chartOnlyAxes.value!);
-
-  new Chart(scales)
-    .addTraces(curvesAxesAndGrid)
-    .addAxes()
-    .addGridLines()
-    .appendTo(chartAxesAndGrid.value!);
-
-  new Chart(scales)
-    .addTraces(curvesAxesLabelsAndGrid)
-    .addAxes({ x: "Time", y: "Value" })
-    .addGridLines()
-    .appendTo(chartAxesLabelsAndGrid.value!);
-
-  new Chart(scales)
-    .addTraces(curvesAxesLabelGridAndZoom)
-    .addAxes({ x: "Time", y: "Value" })
-    .addGridLines()
     .addZoom()
-    .appendTo(chartAxesLabelGridAndZoom.value!);
-
-  new Chart(scales)
-    .addTraces(curvesTooltips)
-    .addTooltips(tooltipHtmlCallback)
-    .appendTo(chartTooltips.value!);
-
-  new Chart(scales)
-    .addTraces(curvesResponsive)
+    .addTraces(makeRandomCurves())
     .addAxes()
-    .addGridLines()
     .makeResponsive()
-    .appendTo(chartResponsive.value!);
+    .addTooltips(tooltipHtmlCallback)
+    .appendTo(chart.value!)
 });
 </script>
