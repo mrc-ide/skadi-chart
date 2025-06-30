124d312c8b1c6d18091e20d61714d1f65dab3c2c
Mantra <mantrakusumgar@outlook.com>
2025-06-30 16:09:52 +0100

    add 2d zoom

--- src/d3.ts
+++ src/d3.ts
@@ -6,5 +6,5 @@ export { select };
 export { type Axis, axisBottom, axisLeft } from "d3-axis";
 export { line, type Line } from "d3-shape";
 export { create, type BaseType, type Selection, type ClientPointEvent, pointer } from "d3-selection";
-export { brushX, type D3BrushEvent } from "d3-brush";
+export { brush, type D3BrushEvent } from "d3-brush";
 export { scaleLinear, type NumberValue, type ScaleLinear } from "d3-scale";
