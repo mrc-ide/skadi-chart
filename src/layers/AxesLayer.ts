9ed5adf3ecbf984ae872fd85deb1f76d6e177457
David Mears <clarinet.david@gmail.com>
2025-10-03 12:51:40 +0100

    Pass layerArgs as argument instead of a property of AxesLayer

--- src/layers/AxesLayer.ts
+++ src/layers/AxesLayer.ts
@@ -10,22 +10,18 @@ type AxisElements = {
 
 export class AxesLayer extends OptionalLayer {
   type = LayerType.Axes;
-  private layerArgs: LayerArgs | null = null;
 
   constructor(public labels: XYLabel) {
     super();
   };
 
-  private drawAxis = (axis: AxisType): AxisElements => {
-    if (!this.layerArgs) {
-      throw new Error("AxesLayer.drawAxis called before layerArgs set");
-    }
-    const { width, height, margin } = this.layerArgs.bounds;
-    const { getHtmlId } = this.layerArgs;
-    const numericalScale = this.layerArgs.scaleConfig.linearScales[axis];
+  private drawAxis = (axis: AxisType, layerArgs: LayerArgs): AxisElements => {
+    const { width, height, margin } = layerArgs.bounds;
+    const { getHtmlId } = layerArgs;
+    const numericalScale = layerArgs.scaleConfig.linearScales[axis];
 
     if (this.labels[axis]) {
-      const label = this.layerArgs.coreLayers[LayerType.Svg].append("text")
+      const label = layerArgs.coreLayers[LayerType.Svg].append("text")
         .attr("id", `${getHtmlId(LayerType.Axes)}-label${axis}`)
         .style("font-size", "1.2rem")
         .attr("text-anchor", "middle")
@@ -42,19 +38,18 @@ export class AxesLayer extends OptionalLayer {
       }
     }
 
-    return this.layerArgs.scaleConfig.categoricalScales[axis]
-      ? this.drawCategoricalAxis(axis)
-      : this.drawNumericalAxis(axis, numericalScale, { ...this.layerArgs.globals.tickConfig[axis], padding: 12 });
+    return layerArgs.scaleConfig.categoricalScales[axis]
+      ? this.drawCategoricalAxis(axis, layerArgs)
+      : this.drawNumericalAxis(axis, numericalScale, { ...layerArgs.globals.tickConfig[axis], padding: 12 }, layerArgs);
   };
 
 
   draw = (layerArgs: LayerArgs) => {
-    this.layerArgs = layerArgs;
-    const { x: scaleX, y: scaleY } = this.layerArgs.scaleConfig.linearScales;
-    const { animationDuration } = this.layerArgs.globals;
+    const { x: scaleX, y: scaleY } = layerArgs.scaleConfig.linearScales;
+    const { animationDuration } = layerArgs.globals;
 
-    const { layer: axisLayerX, axis: axisX, line: axisLineX } = this.drawAxis("x");
-    const { layer: axisLayerY, axis: axisY, line: axisLineY } = this.drawAxis("y");
+    const { layer: axisLayerX, axis: axisX, line: axisLineX } = this.drawAxis("x", layerArgs);
+    const { layer: axisLayerY, axis: axisY, line: axisLineY } = this.drawAxis("y", layerArgs);
 
     // The zoom layer (if added) will update the scaleX and scaleY
     // so axisY and axisX, which are constructed from these will
@@ -93,35 +88,32 @@ export class AxesLayer extends OptionalLayer {
   };
 
   // The amount to translate the axis layer by from the edge of the svg.
-  private translation = (axis: AxisType) => {
-    const svgStartToAxis = this.svgStartToAxis(axis);
+  private translation = (axis: AxisType, layerArgs: LayerArgs) => {
+    const svgStartToAxis = this.svgStartToAxis(axis, layerArgs);
     return {
       [axis]: 0,
       [this.otherAxis(axis)]: svgStartToAxis
     };
   }
 
-  private svgStartToAxis = (axis: AxisType) => {
-    const { height, margin } = this.layerArgs!.bounds;
+  private svgStartToAxis = (axis: AxisType, layerArgs: LayerArgs) => {
+    const { height, margin } = layerArgs.bounds;
     return axis === "x" ? height - margin.bottom : margin.left;
   }
 
-  private drawCategoricalAxis = (axis: AxisType): AxisElements => {
-    if (!this.layerArgs) {
-      throw new Error("AxesLayer.drawAxis called before layerArgs set");
-    }
-    const categoricalScale = this.layerArgs.scaleConfig.categoricalScales[axis]!.main;
-    const { margin } = this.layerArgs.bounds;
-    const svgLayer = this.layerArgs.coreLayers[LayerType.Svg];
-    const { getHtmlId } = this.layerArgs;
-    const { count: tickCount } = this.layerArgs.globals.tickConfig[axis];
+  private drawCategoricalAxis = (axis: AxisType, layerArgs: LayerArgs): AxisElements => {
+    const categoricalScale = layerArgs.scaleConfig.categoricalScales[axis]!.main;
+    const { margin } = layerArgs.bounds;
+    const svgLayer = layerArgs.coreLayers[LayerType.Svg];
+    const { getHtmlId } = layerArgs;
+    const { count: tickCount } = layerArgs.globals.tickConfig[axis];
 
-    const translate = this.translation(axis);
+    const translate = this.translation(axis, layerArgs);
     const bandwidth = categoricalScale.bandwidth();
     const axisCon = this.axisConstructor(axis);
 
     const distanceFromSvgEdgeToAxis = axis === "x" ? margin.bottom : margin.left;
-    const showZeroLine = !this.layerArgs.chartOptions.logScale[axis];
+    const showZeroLine = !layerArgs.chartOptions.logScale[axis];
     const categoricalAxis = axisCon(categoricalScale).ticks(tickCount).tickSize(0)
       .tickPadding(distanceFromSvgEdgeToAxis * (showZeroLine ? 0.3 : 0.2));
     svgLayer.append("g")
@@ -130,14 +122,14 @@ export class AxesLayer extends OptionalLayer {
       .attr("transform", `translate(${translate.x},${translate.y})`)
       .call(categoricalAxis);
 
-    const bandNumericalScales = Object.entries(this.layerArgs!.scaleConfig.categoricalScales[axis]!.bands);
+    const bandNumericalScales = Object.entries(layerArgs.scaleConfig.categoricalScales[axis]!.bands);
     bandNumericalScales.forEach(([category, bandNumericalScale]) => {
-      if (showZeroLine && this.layerArgs!.scaleConfig.categoricalScales.y) {
+      if (showZeroLine && layerArgs.scaleConfig.categoricalScales.y) {
         // Add a tick and label at y=0 for each band, if the y-axis is categorical
-        this.drawNumericalAxis("y", bandNumericalScale, { count: 1, padding: 6 });
+        this.drawNumericalAxis("y", bandNumericalScale, { count: 1, padding: 6 }, layerArgs);
       }
       // Each band gets a line at its ending edge
-      this.drawLinePerpendicularToAxis(axis, categoricalScale(category!)! + bandwidth);
+      this.drawLinePerpendicularToAxis(axis, categoricalScale(category!)! + bandwidth, layerArgs);
     });
 
     return { layer: null, axis: null, line: null }; // No need to return axis elements as this axis won't be zoomed
@@ -147,32 +139,38 @@ export class AxesLayer extends OptionalLayer {
     axis: AxisType,
     scale: ScaleNumeric,
     tickConfig: TickConfig & { padding: number },
+    layerArgs: LayerArgs
   ): AxisElements => {
-    const { getHtmlId } = this.layerArgs!;
+    const { getHtmlId } = layerArgs;
     const { count: tickCount, specifier: tickSpecifier, padding: tickPadding } = tickConfig;
     const axisCon = this.axisConstructor(axis);
-    const translate = this.translation(axis);
+    const translate = this.translation(axis, layerArgs);
     let axisLine: D3Selection<SVGLineElement> | null = null;
 
     const numericalAxis = axisCon(scale).ticks(tickCount, tickSpecifier).tickSize(0).tickPadding(tickPadding);
-    const axisLayer = this.layerArgs!.coreLayers[LayerType.Svg].append("g")
+    const axisLayer = layerArgs.coreLayers[LayerType.Svg].append("g")
       .attr("id", `${getHtmlId(LayerType.Axes)}-${axis}`)
       .style("font-size", "0.75rem")
       .attr("transform", `translate(${translate.x},${translate.y})`)
       .call(numericalAxis);
     axisLayer.select(".domain").style("stroke-opacity", 0);
 
-    if (!this.layerArgs!.chartOptions.logScale[axis]) {
+    if (!layerArgs.chartOptions.logScale[axis]) {
       // Draw a line at [axis]=0
-      axisLine = this.drawLinePerpendicularToAxis(axis, scale(0), "darkgrey");
+      axisLine = this.drawLinePerpendicularToAxis(axis, scale(0), layerArgs, "darkgrey");
     }
 
     return { layer: axisLayer, axis: numericalAxis, line: axisLine };
   }
 
-  private drawLinePerpendicularToAxis = (axis: AxisType, positionSC: number, color: string = "black") => {
-    const baseLayer = this.layerArgs!.coreLayers[LayerType.BaseLayer];
-    const { height, width, margin } = this.layerArgs!.bounds;
+  private drawLinePerpendicularToAxis = (
+    axis: AxisType,
+    positionSC: number,
+    layerArgs: LayerArgs,
+    color: string = "black",
+  ) => {
+    const baseLayer = layerArgs.coreLayers[LayerType.BaseLayer];
+    const { height, width, margin } = layerArgs.bounds;
 
     return baseLayer.append("g").append("line")
       .attr(`${axis}1`, positionSC)
