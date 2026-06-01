d194ce34d2d5274b46c9e5be8eb7cbfb95e4ca85
David Mears <clarinet.david@gmail.com>
2026-06-01 17:31:49 +0100

    Use vue dev tools

--- src/demo/vite.config.ts
+++ src/demo/vite.config.ts
@@ -2,11 +2,12 @@ import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   publicDir: resolve(__dirname, "..", "..", "public"),
   resolve: {
     alias: {
